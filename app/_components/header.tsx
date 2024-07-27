b8ffa8d473bef580dc023a89784a308472dff614
João Victor <123279954+joaodev2005@users.noreply.github.com>
2024-07-27 14:41:08 -0300

    feat: add initial header component

--- app/_components/header.tsx
+++ app/_components/header.tsx
@@ -1,8 +1,42 @@
 
-const header = () => {
-  return (
-    <div>header</div>
-  )
+import { UserCircle } from "lucide-react"
+import { ShoppingCart } from "lucide-react"
+import { Button } from "./ui/button"
+import { Card, CardContent } from "./ui/card"
+
+const Header = () => {
+
+    return (
+        <Card>
+            <CardContent className="bg-[#0B0B0B] flex justify-between items-center px-5 py-8">
+
+                <h2 className="text-xl font-bold">
+                    <span className="text-primary">SHOP</span> MASTER
+                </h2>
+
+                <nav>
+                    <ul className="flex items-center gap-4 text-white">
+                        <li><a href="#" className="hover:text-primary">Início</a></li>
+                        <li className="text-white">|</li>
+                        <li><a href="#" className="hover:text-primary">Catálogo</a></li>
+                        <li className="text-white">|</li>
+                        <li><a href="#" className="hover:text-primary">Ofertas</a></li>
+                    </ul>
+                </nav>
+
+                <div className="flex gap-4">
+                    <Button variant={"outline"} size={"icon"}>
+                        <UserCircle size={18} />
+                    </Button>
+
+                    <Button variant={"outline"} size={"icon"}>
+                        <ShoppingCart size={18} />
+                    </Button>
+                </div>
+
+            </CardContent>
+        </Card>
+    )
 }
 
-export default header
+export default Header
